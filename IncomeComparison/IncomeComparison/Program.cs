d8f9c6aa70af2d79e22900ea05c5612bead79d09
Belvedere-1987 <charles.john87@yahoo.com>
2020-03-30 12:29:09 -0700

    Update Income Comparison User Input
    Added User inputs for incomen comparison

--- IncomeComparison/IncomeComparison/Program.cs
+++ IncomeComparison/IncomeComparison/Program.cs
@@ -6,27 +6,28 @@
         {
         Console.WriteLine("Anonymous Income Comparison Program");
         Console.WriteLine("Person 1");
-        Console.WriteLine("Hourly Rate");
-        int hourlyRatePerson1 = 22;
-        Console.WriteLine(hourlyRatePerson1);
-        Console.WriteLine("Hours Worked");
-        int hoursWorkedPerson1 = 40;
-        Console.WriteLine(hoursWorkedPerson1);
+        Console.WriteLine("Hourly Rate?");
+        string hourlyRatePerson1 = Console.ReadLine();
+        int hourlyRateNumPerson1 = Convert.ToInt16(hourlyRatePerson1);
+        Console.WriteLine("Hours Worked?");
+        string hoursWorkedPerson1 = Console.ReadLine();
+        int hoursWorkedNumPerson1 = Convert.ToInt16(hoursWorkedPerson1);
+                
         
         Console.WriteLine("Person 2");
-        Console.WriteLine("Hourly Rate");
-        int hourlyRatePerson2 = 18;
-        Console.WriteLine(hourlyRatePerson2);
-        Console.WriteLine("Hours Worked");
-        int hoursWorkedPerson2 = 50;
-        Console.WriteLine(hoursWorkedPerson2);
-
+        Console.WriteLine("Hourly Rate?");
+        string hourlyRatePerson2 = Console.ReadLine();
+        int hourlyRateNumPerson2 = Convert.ToInt16(hourlyRatePerson2);
+        Console.WriteLine("Hours Worked?");
+        string hoursWorkedPerson2 = Console.ReadLine();
+        int hoursWorkedNumPerson2 = Convert.ToInt16(hoursWorkedPerson2);
+        
         Console.WriteLine("Annual Salary of Person 1");
-        int annualSalary1 = hourlyRatePerson1 * hoursWorkedPerson1 * 52; 
+        int annualSalary1 = hourlyRateNumPerson1 * hoursWorkedNumPerson1 * 52; 
         Console.WriteLine(annualSalary1);
 
         Console.WriteLine("Annual Salary of Person 2");
-        int annualSalary2 = hourlyRatePerson2 * hoursWorkedPerson2 * 52;
+        int annualSalary2 = hourlyRateNumPerson2 * hoursWorkedNumPerson2 * 52;
         Console.WriteLine(annualSalary2);
 
         Console.WriteLine("Does Person 1 make more money than Person 2?");
