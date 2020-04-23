ff326597429ac71038fdfe2b2ee2cea14f081dc6
Belvedere-1987 <charles.john87@yahoo.com>
2020-04-22 17:10:28 -0700

    Added TwentyOne and ClassAssignment
    Add Twenty one and classAssignment files

---
+++ ClassAssignment/ClassAssignment/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    public class Math
+    {
+        public static int multi(int input)
+        {
+            int multiAnswer = input * 2;
+            return multiAnswer; 
+         }
+
+        public static int divi(int input)
+        {
+            int diviAnswer = input / 2;
+            return diviAnswer;
+        }
+
+        public static int addi(int input)
+        {
+            int addiAnswer = input + 2;
+            return addiAnswer;
+        }
+    }
+}
---
+++ ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What would number would you like to multiply, divide, and add by 2");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Math.multi(input));
+            Console.WriteLine(Math.divi(input));
+            Console.WriteLine(Math.addi(input)); 
+            Console.ReadLine(); 
+        }
+        public int input { get; set; }
+     }
+           
+        
+}
+
