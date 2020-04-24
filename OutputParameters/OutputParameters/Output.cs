bc109a25ea507a002ed23777059607ef752c834f
Belvedere-1987 <charles.john87@yahoo.com>
2020-04-24 13:50:14 -0700

    Added output parameters file
    Added output files for parameters

---
+++ OutputParameters/OutputParameters/Output.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutputParameters
+{
+    public class Output
+    {
+        public void Seperate(int num)
+        {
+            int answer = num / 2;
+            Console.WriteLine(answer);
+        }
+        public void Half(out int x)
+        {
+            Console.WriteLine("What is the new number you would like to divide by 2?");
+            x = Convert.ToInt16(Console.ReadLine()) / 2;
+        }
+        public void Seperate(int num1, int num2)
+        {
+            int sepAnswer = num1 / num2;
+            Console.WriteLine(sepAnswer);
+        }
+
+    }
+
+    public static class Declaration
+    {
+        public static void declare()
+        {
+            Console.WriteLine("I declare that this method is static!");
+        }
+    }
+
+
+}
---
+++ OutputParameters/OutputParameters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutputParameters
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+            Output output = new Output();
+            Console.WriteLine("Give me a number to divide in half");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            output.Seperate(num1);
+            Console.WriteLine("Here is the same result using output parameters ");
+            output.Half(out int answer);
+            Console.WriteLine(answer);
+            Console.WriteLine("Here is the same result using an overloaded method, What is the new number you would like to divide by 2");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            output.Seperate(num2, 2);
+            Declaration.declare(); 
+            Console.ReadLine();
+        }
+        
+    }
+}
