d055b8ba605c7370a7e2a5a4d99ae4fc5f8fe12a
Belvedere-1987 <charles.john87@yahoo.com>
2020-04-24 12:28:47 -0700

    Add Named Parameter drill
    Added Named Parameters drill from Tech Academy's C# course.

---
+++ NamedParameters/NamedParameters/Naming.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedParameters
+{
+    public class Naming
+    {
+        public void update(int num1, int num2)
+        {
+            int answer1 = num1 + 12;
+            Console.WriteLine(num2);
+
+            //I know that the drill didn't ask for this explicitly, put this line in comments in case the answer to math operation
+            //specified in step 1 was needed. 
+            //Console.WriteLine(answer1) 
+
+        }
+    }
+}
---
+++ NamedParameters/NamedParameters/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Naming naming = new Naming();
+            naming.update(12, 50);
+            naming.update(num2: 36, num1: 30);
+            Console.ReadLine(); 
+        }
+    }
+}
