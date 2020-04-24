915b0cc8787ab08da5dc0ac380f7abfc8cd96a47
Belvedere-1987 <charles.john87@yahoo.com>
2020-04-24 12:12:28 -0700

    Added Optional Parameter files
    Finished optional parameter drill from Tech Academy course

---
+++ OptionalParameter/OptionalParameter/Arithmetic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameter
+{
+    public class Arithmetic
+    {
+        public int combine(int input1, int input2 = 0)
+        {
+            int answer = input1 + input2;
+            return answer;
+        }
+    }
+}
---
+++ OptionalParameter/OptionalParameter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arithmetic arithmetic = new Arithmetic();
+            Console.WriteLine("It's time to do some math! Give me two numbers to add together. \nIf no second number is included, the number will be added by 0");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            int optionalInput2;
+            int finalInput;
+            if(int.TryParse(Console.ReadLine(), out optionalInput2))
+            {
+                finalInput = arithmetic.combine(input1, optionalInput2);
+            }
+            else
+            {
+                finalInput = arithmetic.combine(input1);
+            }
+
+            Console.WriteLine(finalInput);
+            Console.ReadLine(); 
+                  }
+    }
+    }
