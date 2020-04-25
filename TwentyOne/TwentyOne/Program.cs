500ac4d4832cd4124bf3de936c6bdfe185b87093
Belvedere-1987 <charles.john87@yahoo.com>
2020-04-24 18:30:39 -0700

    Added ABstract files and updated TwentyOne
    Created files and updated Twenty one

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,29 +10,33 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-
-            Deck deck = new Deck();
-            deck.Shuffle();
-
-
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-
-            Console.WriteLine(deck.Cards.Count);
+            TwentyOneGame game = new TwentyOneGame();
+            game.Players = new List<string> { "Jesse", "Bill", "bob" };
+            game.ListPlayers();
             Console.ReadLine();
+
+            //Deck deck = new Deck();
+            //deck.Shuffle();
+
+
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+
+            //Console.WriteLine(deck.Cards.Count);
+            //Console.ReadLine();
         }
 
 
 
-        public static Deck Shuffle(Deck deck, int times)
-        {
-            for (int i = 0; i < times; i++)
-            {
-                deck = Shuffle(deck);
-            }
-            return deck;
-        }
+        //public static Deck Shuffle(Deck deck, int times)
+        //{
+        //    for (int i = 0; i < times; i++)
+        //    {
+        //        deck = Shuffle(deck);
+        //    }
+        //    return deck;
+        //}
     }
 }
