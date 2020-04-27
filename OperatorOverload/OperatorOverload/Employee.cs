2ac23497081f26bb80aef9a77e1609cfff792032
Belvedere-1987 <charles.john87@yahoo.com>
2020-04-27 16:08:12 -0700

    Add OperatorOverload file
    Added Operator Overload file

---
+++ OperatorOverload/OperatorOverload/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverload
+{
+    public class Employee : Person, IQuittable
+    {
+        public int Id { get; set; }
+        public override void SayName()
+        {
+            base.SayName();
+            Console.WriteLine("This is being printed from the Employee Class");
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("I quit!");
+
+        }
+
+        public static bool operator == (Employee first, Employee second)
+        {
+        bool status = false;
+            if (first.Id == second.Id)
+            {
+                status = true;
+            }
+            return status;
+        }
+
+        public static bool operator != (Employee first, Employee second)
+        {
+            bool status = false; 
+            if (first.Id != second.Id)
+            {
+                status = true;
+            }
+            return status;
+        }
+    }
+}
---
+++ OperatorOverload/OperatorOverload/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee();
+            Employee employee3 = new Employee(); 
+            
+            employee1.FirstName = "Sample";
+            employee1.LastName = "Student";
+            employee1.Id = 5777;
+            employee1.SayName();
+
+            employee2.FirstName = "Example";
+            employee2.LastName = "Employee";
+            employee2.Id = 5777;
+            employee2.SayName();
+
+            employee3.FirstName = "Charles";
+            employee3.LastName = "Johnson";
+            employee3.Id = 1922;
+            employee3.SayName(); 
+
+            string answer1 = Convert.ToString(employee1 == employee2);
+            string answer2 = Convert.ToString(employee2 == employee3);
+
+            Console.WriteLine("Are employees 1 and 2 the same?");
+            Console.WriteLine(answer1);
+
+            Console.WriteLine("Are employees 2 and e the same?");
+            Console.WriteLine(answer2);
+
+            Console.ReadLine();
+        }
+    }
+}
