d15e86be7dee59258d145f55d8bb13c9a866d75c
Belvedere-1987 <charles.john87@yahoo.com>
2020-04-27 13:54:23 -0700

    Added Interface file
    Added Interface file

---
+++ Interfaces/Interfaces/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+        public class Employee : Person, IQuittable
+        {
+            int Id { get; set; }
+            public override void SayName()
+            {
+                base.SayName();
+                Console.WriteLine("This is being printed from the Employee Class");
+            }
+
+            public void Quit()
+            {
+            Console.WriteLine("I quit!");
+
+
+            }
+        }
+}
