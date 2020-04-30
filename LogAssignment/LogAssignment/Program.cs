4e4ff308a32c68ddc44fb6e4952037cb68d8db4d
Belvedere-1987 <charles.john87@yahoo.com>
2020-04-30 11:56:03 -0700

    Added Log Assignment updatted log for TwentyOne Game

---
+++ LogAssignment/LogAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LogAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite number? ");
+           string answer = Console.ReadLine();
+            using (StreamWriter log = new StreamWriter(@"C:\Users\Student\Documents\GitHub\C--Basic-Coding-Projects\LogAssignment.txt", true))
+            {
+                log.WriteLine(answer); 
+            }
+            using (StreamReader response = new StreamReader(@"C:\Users\Student\Documents\GitHub\C--Basic-Coding-Projects\LogAssignment.txt"))
+            {
+                string line;
+                while ((line = response.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            Console.ReadLine();
+        } 
+    }
+}
