e2671611be6951c82812ca32d2b47a7eb3ee0505
Belvedere-1987 <charles.john87@yahoo.com>
2020-05-20 16:02:26 -0700

    CarInsuranceMVC
    Added and Finsiehd Car Insurance

--- DateTimeAssignment/DateTimeAssignment/Program.cs
+++ DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -13,9 +13,23 @@ namespace DateTimeAssignment
             Console.WriteLine(timeNow);
             Console.WriteLine("Type a number!");
             int timeMultiplier = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(timeNow.AddHours(timeMultiplier)); 
-                        
+      //      Console.WriteLine(timeNow.AddHours(timeMultiplier));
+            DateTime otherDate = timeNow.AddHours(timeMultiplier);
+            DateTime DOB = new DateTime(2001, 5, 16);
+
+            Console.WriteLine(DOB);
+            Console.WriteLine(timeNow);
+            int age = 0; 
+            age = timeNow.Year - DOB.Year;
+            if (timeNow.Month < DOB.Month)
+            { age = age - 1; }
+            else if (timeNow.Month == DOB.Month||timeNow.Day < DOB.Day)
+            { age = age - 1; }
+            if (age <= 25)
+            { Console.WriteLine(25); }
+            Console.WriteLine(age);
             Console.ReadLine(); 
+
 
         }
     }
